f7e83c9712e1b4eb691723fad1238a6e49751cb5
player <mail@mail.com>
2014-12-05 12:30:18 +0300

    fix 1

--- test.py
+++ test.py
@@ -1,15 +1,4 @@
-a = input('Введите а:')
-b = input('Введите b:')
-c = input('Введите c:')
-if len(a) > 0 and len(b) > 0 and len(c) > 0 and not a.isalpha() and not b.isalpha() and not c.isalpha():
-	d = int(b) ** 2 - 4 * int(a) * int(c)
-	if d > 0 or d == 0:
-		import math
-		dd = math.sqrt(d)
-		x = ( -int(b) - int(dd) ) / (int(a) * 2)
-		x1 = ( -int(b) + int(dd) ) / (int(a) * 2)
-		print (x, x1)
-	else:
-		print ('Рещений не имеет')
-else:
-	print ('Введите только числа')
+print("Jack and Jill went up a hill")
+print("to fetch a pail of water;")
+print("Jack fell down, and broke his crown,")
+print("and Jill came tumbling after.")
